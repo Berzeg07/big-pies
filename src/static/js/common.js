247980bb480268ec8db8353979ddb016bc0cd819
Aslan <berezg86@gmail.com>
2021-04-30 20:05:09 +0300

    карточки с пирогами

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -71,5 +71,21 @@ $(document).ready(function () {
         },
     });
 
+    $(".switch-price span").on("click", function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        }
+        else {
+            if (oldValue > 1) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
 
 });
