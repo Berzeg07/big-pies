18889ec3d3b9bbdb5b19315282316805010d7c4e
Aslan <berezg86@gmail.com>
2021-04-30 11:27:52 +0300

    Верстка шапки

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,31 @@
 $(document).ready(function () {
-    // alert(1);
+    $('.select select').selectric();
+
+    $('.burger').on('click', function () {
+        $(this).toggleClass('is-active')
+        $('.dropdownmenu').toggleClass('is-active')
+    })
+
+    $(document).mouseup(function (e) {
+        var div = $(".dropdownmenu");
+        if (div.hasClass('is-active')) {
+            if (!div.is(e.target) && div.has(e.target).length === 0 && !$('.burger').is(e.target)) {
+                div.removeClass('is-active');
+                $('.burger').removeClass('is-active')
+            }
+        }
+    });
+
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= 170) {
+                $('.header-sticky').addClass('is-active');
+            }
+            else {
+                $('.header-sticky').removeClass('is-active');
+            }
+        });
+    });
+
+
 });
