477271b2d54f52c95fcbae01014b2a56141c7e0d
Aslan <berezg86@gmail.com>
2021-04-30 14:32:44 +0300

    2 блока на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -27,5 +27,49 @@ $(document).ready(function () {
         });
     });
 
+    var productGallery = new Swiper('.product-gallery', {
+        slidesPerView: 11,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 2,
+            },
+            767: {
+                slidesPerView: 3,
+            },
+            991: {
+                slidesPerView: 4,
+            },
+            1279: {
+                slidesPerView: 5,
+            },
+            1365: {
+                slidesPerView: 7,
+            },
+            1599: {
+                slidesPerView: 9,
+            },
+        }
+    });
+
+    var bannerGallery = new Swiper('.banner-slider', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
 
 });
